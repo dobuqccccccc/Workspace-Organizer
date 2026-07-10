006d75f7975f6fd6cb7eebdd268ce5692cfdf907
dobuqccccccc <dhy4y94skz@privaterelay.appleid.com>
2026-07-10 17:57:41 +0800

    Add files via upload

---
+++ WorkspaceOrganizer/WorkspaceOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkspaceOrganizer
+//
+//  Created by luosi on 2026/7/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
